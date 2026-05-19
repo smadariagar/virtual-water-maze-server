a71a28ff4da3bd7d31ae95194ea2a51ff8f60359
Samuel Madariaga <smadariagar@ug.uchile.cl>
2026-05-19 17:25:21 -0400

    last update

--- servidor_eyelink.py
+++ servidor_eyelink.py
@@ -1,6 +1,6 @@
 import socket
 import pylink
-import threading
+import sys
 
 # Configuración
 UDP_IP = "127.0.0.1"
@@ -11,31 +11,54 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, PORT_PYTHON))
 
 class GodotCalDisplay(pylink.EyeLinkCustomDisplay):
-    def draw_cal_target(self, x, y):
-        # Envía las coordenadas del punto a Godot
-        msg = f"CAL_DRAW,{int(x)},{int(y)}"
-        sock.sendto(msg.encode(), (UDP_IP, PORT_GODOT))
+    def __init__(self):
+        super().__init__()
+
+    # --- Funciones obligatorias del SDK ---
+    def setup_cal_display(self):
+        sock.sendto(b"CAL_START", (UDP_IP, PORT_GODOT))
+
+    def exit_cal_display(self):
+        sock.sendto(b"CAL_END", (UDP_IP, PORT_GODOT))
 
     def clear_cal_display(self):
         sock.sendto(b"CAL_ERASE", (UDP_IP, PORT_GODOT))
 
-# Inicialización
+    def draw_cal_target(self, x, y):
+        msg = f"CAL_DRAW,{int(x)},{int(y)}"
+        sock.sendto(msg.encode(), (UDP_IP, PORT_GODOT))
+
+    def erase_cal_target(self):
+        sock.sendto(b"CAL_ERASE", (UDP_IP, PORT_GODOT))
+
+    def get_input_key(self):
+        # Intentamos leer si Godot nos envió una tecla
+        sock.setblocking(False) # No congelar
+        try:
+            data, addr = sock.recvfrom(1024)
+            msg = data.decode()
+            if msg == "ACCEPT_CAL":
+                return [pylink.KeyInput(32, 0)] # Espacio
+            elif msg == "ESC_CAL":
+                return [pylink.KeyInput(27, 0)] # ESC
+        except:
+            pass
+        sock.setblocking(True)
+        return None
+
+# --- Ejecución ---
 el_tracker = pylink.EyeLink(None) # Usa None para modo Dummy
 pylink.openGraphicsEx(GodotCalDisplay())
 
-def escuchar_godot():
-    while True:
-        data, addr = sock.recvfrom(1024)
-        msg = data.decode()
-        if msg == "CALIBRATE":
-            el_tracker.doTrackerSetup()
-            sock.sendto(b"CAL_END", (UDP_IP, PORT_GODOT))
-        # Aquí puedes añadir más lógica para eventos de trial
-        print(f"Comando recibido: {msg}")
+el_tracker.openDataFile("test.edf")
+el_tracker.startRecording(1, 1, 1, 1)
 
-# Hilo en segundo plano
-thread = threading.Thread(target=escuchar_godot, daemon=True)
-thread.start()
+print("Servidor Python listo...")
 
-print("Servidor listo. Esperando comandos...")
-input("Presiona Enter para cerrar.\n")
+# Aquí recibimos el comando para CALIBRAR
+while True:
+    data, addr = sock.recvfrom(1024)
+    if data.decode() == "CALIBRATE":
+        print("Iniciando calibración...")
+        el_tracker.doTrackerSetup()
+        print("Calibración terminada.")
